2e2558ea34907b2d4d5ec94fc834c3c0b6518d38
Patrick Post <www.drummer_456@hotmail.com>
2020-08-29 11:20:23 +0200

    Setup of my portfolio made with Spring boot. Used a template from templated.co and made it work with thymeleaf.

---
+++ src/test/java/com/example/sortingAlgorithms/SortingAlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sortingAlgorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortingAlgorithmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
